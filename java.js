bc45b28a3c2a9d2a25f19666d6cf13b8e1c2bfff
HECTOR <al45979@un.edu.mx>
2026-07-02 20:44:06 -0600

    Update java.js

--- java.js
+++ java.js
@@ -1,44 +1,12 @@
-body{
-    background-color: #d9d9d9;
-    font-family: Arial, Helvetica, sans-serif;
+function agregar(numero){
+    document.getElementById("pantalla").value += numero;
 }
 
-.calculadora{
-    width: 280px;
-    margin: 60px auto;
-    background: white;
-    padding: 20px;
-    border-radius: 10px;
-    box-shadow: 0 0 8px gray;
-    text-align: center;
+function limpiar(){
+    document.getElementById("pantalla").value = "";
 }
 
-h2{
-    margin-bottom: 15px;
-}
-
-#pantalla{
-    width: 95%;
-    height: 40px;
-    font-size: 20px;
-    text-align: right;
-    margin-bottom: 15px;
-}
-
-.botones{
-    display: grid;
-    grid-template-columns: repeat(4,1fr);
-    gap: 10px;
-}
-
-button{
-    height: 45px;
-    font-size: 18px;
-    cursor: pointer;
-}
-
-.limpiar{
-    grid-column: span 4;
-    background: crimson;
-    color: white;
+function calcular(){
+    var resultado = eval(document.getElementById("pantalla").value);
+    document.getElementById("pantalla").value = resultado;
 }
